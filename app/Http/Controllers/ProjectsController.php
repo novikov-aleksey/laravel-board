2cb3280037767b9abe141150db9722bf7775f4e4
Aleksey Novikov <Novikov_9292@mail.ru>
2019-07-01 17:04:43 +0600

    Added project view route

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -7,22 +7,33 @@ use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $projects = Project::all();
         return view('projects.index', compact('projects'));
     }
 
+    /**
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function store()
     {
-        request()->validate([
-            'title' => 'required',
-            'description' => 'required',
-        ]);
-
-        Project::create(request(['title', 'description']));
-
+        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+        Project::create($attributes);
 
         return redirect('/projects');
+    }
+
+    /**
+     *
+     * @param \App\Project $project
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
     }
 }
