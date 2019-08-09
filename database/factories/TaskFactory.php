7d0f93e8b301dd90bdf30a49b7edb1a863a179d5
Aleksey Novikov <Novikov_9292@mail.ru>
2019-08-09 11:15:39 +0600

    Added Task factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use App\Task;
+use Faker\Generator as Faker;
+
+$factory->define(Task::class, function (Faker $faker) {
+    return [
+        'body' => $faker->sentence
+    ];
+});
