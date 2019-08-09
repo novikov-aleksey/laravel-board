70a5f07240e3cacc89f074a9aebc0ddfec6314f0
Aleksey Novikov <Novikov_9292@mail.ru>
2019-08-09 10:37:10 +0600

    Added basic UI to the Project page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -11,15 +11,7 @@
     <main class="flex flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
-                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-light-blue pl-4">
-                       <a class="text-xl text-base no-underline" href="{{$project->path()}}">{{$project->title}}</a>
-                    </h3>
-
-                    <div class="text-grey">
-                        {{\Illuminate\Support\Str::limit($project->description, 300)}}
-                    </div>
-                </div>
+                @include('projects.card')
             </div>
         @empty
             <div>No projects yet</div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,9 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$project->title}}</h1>
-    <div>{{$project->description}}</div>
-    <a href="/projects">Go Back</a>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <p class="text-grey text-sm text-normal no-underline">
+                <a href="/projects" class="text-grey text-sm text-normal no-underline">My Projects</a>  / {{$project->title}}
+            </p>
+            <a class="button" href="/projects/create">Create</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3">
+                {{--tasks--}}
+                <div class="mb-6">
+                    <h2 class="text-grey text-normal text-lg mb-3">Tasks</h2>
+                    <div class="mb-3 card">Task 1</div>
+                    <div class="mb-3 card">Task 2</div>
+                    <div class="mb-3 card">Task 3</div>
+                    <div class="card">Task 4</div>
+                </div>
+
+                {{-- general notes --}}
+                <div >
+                    <h2 class="text-grey text-normal text-lg mb-3">General notes</h2>
+
+                    <textarea class="card w-full" style="min-height: 200px">General notes</textarea>
+                </div>
+            </div>
+            <div class="lg:w-1/4 px-3">
+                @include('projects.card')
+            </div>
+        </div>
+    </main>
 @endsection
 
 
